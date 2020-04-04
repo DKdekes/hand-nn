743e75a264e64c3bcbe2f4ba27b86b8188c98580
dkdekes <eric_walden@baylor.edu>
2020-04-04 08:05:53 -0700

    handle single examples in squared_error

--- hand/error/squared_error.py
+++ hand/error/squared_error.py
@@ -1,8 +1,12 @@
 import numpy as np
 
 
-def error(o, t):
-    return np.sum(0.5 * np.square(o - t))
+def error(y, y_p):
+    if y.shape == ():
+        y = np.array([y])
+    if y_p.shape == ():
+        y_p = np.array([y_p])
+    return np.sum(np.square(y - y_p))
 
 
 if __name__ == '__main__':
